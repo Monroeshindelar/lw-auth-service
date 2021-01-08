49630da75090d06c0f0e9eb676e0820283ffffe4
monroeshindelar <monroeshindelar@gmail.com>
2021-01-07 21:21:14 -0800

    Initial commit

---
+++ src/test/java/com/tm/tmapi/TmapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.tmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
